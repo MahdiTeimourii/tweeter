b1d52180591279d8b7fbe56c2f8c8109d8e75fc1
Mahdi Teimouri <mahditei@mail.com>
2023-05-29 18:08:54 -0700

    char counter successfully tracks

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,22 @@
 $(document).ready(function () {
-  $('.new-tweet textarea').on('input', function () {
+
+  $('textarea').on('input', function () {
+    // Calculate the length of the input text
     var inputLength = $(this).val().length;
-    var counter = $(this).closest('.new-tweet').find('counter');
+    var container = $(this).closest('.new-tweet')
+    console.log(container)
+    // Find the counter element within the closest '.new-tweet' parent
+    var counter = $(this).closest('.new-tweet').find('.counter');
+    // Update the counter text to display the remaining characters
     counter.text(140 - inputLength);
+
+    // Check if the input length exceeds the character limit (140)
     if (inputLength > 140) {
-      counter.addClass('invalid');
+      // If the input is too long, add the 'invalid' class to the counter element
+      counter.addClass('error');
     } else {
-      counter.removeClass('invalid');
+      // If the input is within the limit, remove the 'invalid' class from the counter element
+      counter.removeClass('error');
     }
   });
 });
